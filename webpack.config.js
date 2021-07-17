fd98c310acdef6c7a8f741a8b17aad1c192ef93f
Vlad <vladhillary@gmail.com>
2021-07-17 16:52:10 +0300

    added some plugins for webpack dependences

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,33 @@
 const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     mode: 'development',
-    entry: './index.html',
+    entry: {
+        main: path.resolve(__dirname, './index.html'),
+        article: path.resolve(__dirname, './page/article.html.html'),
+        createArticle: path.resolve(__dirname, './page/createArticle.html'),
+        singIn: path.resolve(__dirname, './page/singIn.html')
+    },
     output: {
-        filename: 'app.js',
+        filename: '[name].js',
         path: path.resolve(__dirname, 'bundle'),
     },
     plugins: [
-        new HtmlWebpackPlugin({ template: './pages/singIn.html'}),
-        new HtmlWebpackPlugin({ template: './pages/article.html'}),
-        new HtmlWebpackPlugin({ template: './pages/createArticle.html'})
-    ]
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({ template: './pages/singIn.html' }),
+        new HtmlWebpackPlugin({ template: './pages/article.html' }),
+        new HtmlWebpackPlugin({ template: './pages/createArticle.html' })
+    ],
+    module: {
+        rules: [
+            {
+                test:/\.css$/,
+                use: [
+
+                ]
+            }
+        ]
+    }
 };
